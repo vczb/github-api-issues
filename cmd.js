d69596394800963c8f69ac2b29ea1887f9b14c14
Vinicius CFGroup <vz@cfgroup.com.br>
2020-01-22 22:41:50 -0300

    used commands

---
+++ cmd.js
@@ -0,0 +1,10 @@
+// Criando um novo projeto
+create-react-app new-project
+// Instalando as rotas
+npm install react-router-dom
+// Instalando Estilos
+npm install styled-components
+// Instalando ícones
+npm install react-icons
+// Instala Axios para consumir API
+npm install axios
