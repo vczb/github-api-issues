803358ade549888eddffca4599cc9e32b8e738b2
Vinicius CFGroup <vz@cfgroup.com.br>
2020-01-23 21:57:57 -0300

    Link to={`/repository/${ encodeURIComponent(repo.name) }`}

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,6 +1,6 @@
 import React, {useState, useCallback, useEffect} from 'react';
 import {FaGithub, FaPlus, FaSpinner, FaBars, FaTrash} from 'react-icons/fa';
-
+import {Link} from 'react-router-dom';
 import {Container, Form, SubmitButton, List, DeleteButton} from './styles';
 
 import api from '../../services/api';
@@ -109,9 +109,9 @@ export default function Main(){
             </DeleteButton>
             {repo.name}
           </span>
-          <a href="" >
+          <Link to={`/repository/${ encodeURIComponent(repo.name) }`} >
             <FaBars size={20}/>
-          </a>
+          </Link>
         </li>
       ))}
     </List>
